53e99a200d5ab2891186a3c4377134d4e53678cf
carlos  andres cordoba <ancoro2009@gmail.com>
2022-08-21 15:39:37 -0500

    update

--- Simulator.js
+++ Simulator.js
@@ -9,27 +9,27 @@ const Task = require("./Task.js")
  * @returns number of tasks inand time spent 
  */
 const simulator = (numSeconds, pagesPerMinute) => {
-    let labPrinter = new Printer(pagesPerMinute)
-    let printQueue = new Queue()
+    let labPrinter = new Printer(pagesPerMinute) //Object that instantiates the Printer Class
+    let printQueue = new Queue()//Object that instantiates the queue class
     let waitTimes = []
 
-    for (let currenSecond = 0; currenSecond <= numSeconds; currenSecond++) {//
-        if (newPrintTask()) {
+    for (let currenSecond = 0; currenSecond <= numSeconds; currenSecond++) {//time course allotted in seconds
+        if (newPrintTask()) {// condition that instantiates the task class and enters a new task, assigning it the time corresponding to its entry into the queue
             let task = new Task(currenSecond)
 
             printQueue.enqueue(task)
         }
-        if ((!labPrinter.busy()) && (!printQueue.isEmpty())) {
+        if ((!labPrinter.busy()) && (!printQueue.isEmpty())) {//condition that checks for the existence of tasks and extracts the initial task from the queue
             let newTask = printQueue.dequeue()
 
             waitTimes.push(newTask.waitTime(currenSecond))
             labPrinter.startNext(newTask)
 
         }
-        labPrinter.tick()
+        labPrinter.tick()//execution of the tick method of the printer class to calculate the time and existence of print jobs
     }
 
-    let averageWait = waitTimes.reduce((acc, b) => acc + b, 0) / (waitTimes.length)
+    let averageWait = waitTimes.reduce((acc, b) => acc + b, 0) / (waitTimes.length)//average time for tasks to remain in the queue
 
 
     return `Average Wait: ${averageWait.toFixed(2)} secs, ${printQueue.size()} tasks remaining`
@@ -52,6 +52,11 @@ const newPrintTask = () => {
 
 }
 
+/**
+ * execution of the simulator function  
+ * @param time in seconds 
+ * @param  number  of  pages a print
+ */
 for (let i = 0; i <= 10; i++) {
 
     console.log(simulator(3600, 5))
